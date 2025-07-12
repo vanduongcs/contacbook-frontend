32a6ed0149992b95195e0fd7db95d5ce035dd713
vanduongcs <ddvanworkmail@gmail.com>
2025-07-12 23:34:26 +0700

    Hien thi danh sasch cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/js/all.min.js";
+import router from "./router";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        vueDevTools(),
-    ],
+    plugins: [vue()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
-        },
+        }
     },
     server: {
-        port: 3001
-    }
-})
+        port: 3001,
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000/",
+                changeOrigin: true,
+            },
+        }
+    },
+});
